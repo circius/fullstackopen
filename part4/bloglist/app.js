67656f2b484eb5aaff4cfcae405211173ed2d091
circius <circius@posteo.de>
2020-10-01 18:04:46 +0200

    refactored bloglist per best practice, 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
+const config = require("./config/config");
+const { app } = require("./index");
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+app.use(cors());
+app.use(express.json());
+app.use('/api/blogs', blogsRouter);
