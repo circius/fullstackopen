25bb57b687e8f2b1a3c5aaf3908419a89ba38ff4
circius <circius@posteo.de>
2020-10-01 15:45:39 +0200

    eslint fixes (whitespace and quotes)

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -13,81 +13,81 @@ morgan.token('content', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 const errorResponse = (res, statusCode, error) =>
-    res.status(statusCode).json({ 'error': error })
+  res.status(statusCode).json({ 'error': error })
 
 app.get('/api/persons', (req, res, next) => {
-    Entry.find({})
-        .then(result => res.json(result))
-        .catch(error => next(error))
+  Entry.find({})
+    .then(result => res.json(result))
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const person = req.body
+  const person = req.body
 
-    const validPersonP = person => person.name && person.number
+  const validPersonP = person => person.name && person.number
 
-    if (!validPersonP(person)) {
-        return errorResponse(res, 400, 'invalid person. supply name and number.')
-    }
+  if (!validPersonP(person)) {
+    return errorResponse(res, 400, 'invalid person. supply name and number.')
+  }
 
-    const entry = new Entry(person)
-    entry.save()
-        .then(result => res.json(result))
-        .catch(error => next(error))
+  const entry = new Entry(person)
+  entry.save()
+    .then(result => res.json(result))
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    console.log(`getting person with id ${id}`)
-    Entry.findById(id)
-        .then(person => 
-            person ? res.json(person) : res.status(404).end())
-        .catch(error => next(error))
+  const id = req.params.id
+  console.log(`getting person with id ${id}`)
+  Entry.findById(id)
+    .then(person => 
+      person ? res.json(person) : res.status(404).end())
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    const person = req.body
-    Entry.findByIdAndUpdate(id, person, {new:true, runValidators: true})
-        .then(result => res.json(result))
-        .catch(error => next(error))
+  const id = req.params.id
+  const person = req.body
+  Entry.findByIdAndUpdate(id, person, {new:true, runValidators: true})
+    .then(result => res.json(result))
+    .catch(error => next(error))
 })
 
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    Entry.findByIdAndDelete(id)
-        .then(res.status(204).end())
-        .catch(error => next(error))
+  const id = req.params.id
+  Entry.findByIdAndDelete(id)
+    .then(res.status(204).end())
+    .catch(error => next(error))
 })
 
 app.get('/info', (req, res, next) => {
-    Entry.find({})
-        .then(result => {
-            const phonebookEntries = result.length
-            const info = `Phonebook has info for ${phonebookEntries} people`
-            const timestamp = new Date()
-            res.send(`${info}<br /><br />${timestamp}`)
-        })
-        .catch(error => next(error))
+  Entry.find({})
+    .then(result => {
+      const phonebookEntries = result.length
+      const info = `Phonebook has info for ${phonebookEntries} people`
+      const timestamp = new Date()
+      res.send(`${info}<br /><br />${timestamp}`)
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return errorResponse(res, 400, 'malformed id')
-    } else if (error.name === 'ValidationError') {
-        return errorResponse(res, 400, 'invalid input')
-    }
+  if (error.name === 'CastError') {
+    return errorResponse(res, 400, 'malformed id')
+  } else if (error.name === 'ValidationError') {
+    return errorResponse(res, 400, 'invalid input')
+  }
 
-    next(error)
+  next(error)
 }
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log('Server running on port ', PORT)
+  console.log('Server running on port ', PORT)
 })
 
 
--- part3/phonebook-backend/modules/entries.js
+++ part3/phonebook-backend/modules/entries.js
@@ -5,31 +5,31 @@ const mongoURI = process.env.MONGODB_URI
 console.log(`connecting to ${mongoURI}`)
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
-    .then(res => console.log('connected to mongoDB'))
-    .catch(error => console.log('error connecting to MongoDB:', error.message))
+  .then(res => console.log('connected to mongoDB'))
+  .catch(error => console.log('error connecting to MongoDB:', error.message))
 
 const entrySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        unique: true,
-        minlength: 3,
-        required:true
-    },
-    number: {
-        type: String,
-        minlength: 8,
-        required:true,
-    }
+  name: {
+    type: String,
+    unique: true,
+    minlength: 3,
+    required:true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required:true,
+  }
 })
 
 entrySchema.plugin(uniqueValidator)
 
 entrySchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Entry', entrySchema)
