ea5a52720f521a97119e703f80eff30f851d0a39
circius <circius@posteo.de>
2020-10-01 18:07:58 +0200

    changed artefact from autorefactoring

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,9 +3,11 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const config = require("./config/config");
-const { app } = require("./index");
+const app = express()
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
 app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
+
+module.exports = app
