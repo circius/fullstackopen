8cf494de143c0d43086160532040b2f17c0f8575
circius <circius@posteo.de>
2020-10-02 12:49:53 +0200

    utils/favoriteBlog with tests

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -1,8 +1,52 @@
+const { TestScheduler } = require('jest')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+const blog1 = {
+  "_id": "5f75fec799dfaa36716c15ca",
+  "title": "whatever",
+  "author": "whoever",
+  "url": "example.com",
+  "likes": 0,
+  "__v": 0
+}
+const blog2 = {
+  "_id": "5f76fe485c8ddd651fd91ca7",
+  "title": "another",
+  "author": "whoever",
+  "url": "beispiel.com",
+  "likes": 3,
+  "__v": 0
+}
+const blog3 = {
+  "_id": "5f76fe705c8ddd651fd91ca8",
+  "title": "last",
+  "author": "jerome",
+  "url": "example.fr",
+  "likes": 2,
+  "__v": 0
+}
+const blogs0 = []
+const blogs1 = [blog1]
+const blogs2 = [blog1, blog2]
+const blogs3 = [blog1, blog2, blog3]
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+describe('total likes', () => {
+  test('of empty list is zero', () =>{
+    expect(listHelper.totalLikes(blogs0)).toBe(0)
+  })
+  test('of bloglist of 1 is the likes of the solitary member', () =>
+    expect(listHelper.totalLikes(blogs1)).toBe(0))
+  test('varies depending on likes of blogs', () => {
+    expect(listHelper.totalLikes(blogs2)).toBe(3)
+    expect(listHelper.totalLikes(blogs3)).toBe(5)
+  })
+})
+
+describe('favorite blog', () => {
+  test('of empty bloglist is undefined', () => 
+    expect(listHelper.favoriteBlog(blogs0)).toBe(undefined))
+  test('of bloglist of 1 is the solitary member', () =>
+    expect(listHelper.favoriteBlog(blogs1)).toEqual(blog1))
+  test('of longer bloglist is the most liked', () => 
+    expect(listHelper.favoriteBlog(blogs3)).toEqual(blog2))
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,15 @@
-const dummy = blogs => 1
+const totalLikes = blogs => blogs.reduce(
+  (acc, blog) => acc + blog.likes, 0
+)
+
+const favoriteBlog = blogs => 
+  blogs.length === 0 ? 
+  undefined : 
+  blogs.reduce(
+    (acc, cur) => cur.likes > acc.likes ? cur : acc
+)
 
 module.exports = {
-  dummy
+  totalLikes,
+  favoriteBlog
 }
