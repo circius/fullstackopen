ad0bcd27f170c0ec99f04e0b88f163861f3e64a8
circius <circius@posteo.de>
2020-10-03 21:41:34 +0200

    _id -> id , kill _v ; tests and code for this

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
-const { blogs3 } = require('./blog_td')
+const { blog1, blogs3 } = require('./blog_td')
 
 const api = supertest(app)
 
@@ -23,6 +23,16 @@ describe('can get list of blogs', () => {
   test('returns right number of blogs', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body).toHaveLength(3)
+  })
+})
+
+describe('blogs are correctly formatted', () => {
+  test('blogs have `id` not `_id`', async () => {
+    const blog1ID = blog1._id
+    const blog0FromDb = await api.get(`/api/blogs/${blog1ID}`)
+    expect(blog0FromDb.id).toBeDefined()
+    expect(blog0FromDb._id).not().toBeDefined()
+
   })
 })
 
--- part4/bloglist/tests/blog_td.js
+++ part4/bloglist/tests/blog_td.js
@@ -1,26 +1,23 @@
 const blog1 = {
-  _id: '5f75fec799dfaa36716c15ca',
+  id: '5f75fec799dfaa36716c15ca',
   title: 'whatever',
   author: 'whoever',
   url: 'example.com',
   likes: 0,
-  __v: 0,
 }
 const blog2 = {
-  _id: '5f76fe485c8ddd651fd91ca7',
+  id: '5f76fe485c8ddd651fd91ca7',
   title: 'another',
   author: 'whoever',
   url: 'beispiel.com',
   likes: 3,
-  __v: 0,
 }
 const blog3 = {
-  _id: '5f76fe705c8ddd651fd91ca8',
+  id: '5f76fe705c8ddd651fd91ca8',
   title: 'last',
   author: 'jerome',
   url: 'example.fr',
   likes: 2,
-  __v: 0,
 }
 const blogs0 = []
 const blogs1 = [blog1]
