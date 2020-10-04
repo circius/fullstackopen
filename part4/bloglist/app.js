71f9a6dbf8399b184c38ede6cbe7fd7b46e19e54
circius <circius@posteo.de>
2020-10-04 12:23:34 +0200

    added error handler middleware

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,14 +3,18 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const config = require('./config/config')
+const errorHandler = require('./middleware/errorHandler')
 
 const app = express()
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true,
 })
+
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use(errorHandler)
+
 
 module.exports = app
---
+++ part4/bloglist/middleware/errorHandler.js
@@ -0,0 +1,16 @@
+const errorHandler = (error, _, res, next) => {
+  const errorResponse = (res, statusCode, error) =>
+    res.status(statusCode).json({ 'error': error })
+
+    process.env.NODE_ENV !== 'test' ? 
+    console.error('error: ', error.message) :
+    null
+
+  if (error.name === 'ValidationError') {
+    return errorResponse(res, 400, 'invalid input')
+  }
+
+  next(error)
+}
+
+module.exports = errorHandler
