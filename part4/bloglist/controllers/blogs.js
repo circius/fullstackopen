0a32b66369edecf67e4ee3ba4904c2eb65ab4417
circius <circius@posteo.de>
2020-10-04 12:24:01 +0200

    fixed POST logic

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -6,10 +6,20 @@ notesRouter.get('/', async (_, response) => {
   response.json(blogs)
 })
 
-notesRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
-  blog.save()
-  response.status(201).json(blog)
+notesRouter.post('/', async (request, response, next) => {
+  const body = request.body
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch(exception) {
+    next(exception)
+  }
 })
 
 module.exports = notesRouter
