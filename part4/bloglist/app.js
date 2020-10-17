102d20a755b5de77a6bdb54d1b7d78a512c16552
circius <circius@posteo.de>
2020-10-17 18:20:22 +0200

    outline of remaining user functionality

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 require('express-async-errors')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users.js')
 const config = require('./config/config')
 const errorHandler = require('./middleware/errorHandler')
 
@@ -16,6 +17,7 @@ mongoose.connect(config.MONGODB_URI, {
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(errorHandler)
 
 
