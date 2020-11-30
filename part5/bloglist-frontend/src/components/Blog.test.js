b19c5e39f57daa5ff1a04906f8d073344bf9d5f5
circius <circius@posteo.de>
2020-11-30 15:36:19 +0100

    5.14 tests for BlogInfo

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -26,14 +26,23 @@ const blog1 = {
 
 describe('<Blog />', () => {
   test('by default, shows author and title but no other details', () => {
-    const mockUpdate = jest.fn()
-    const mockDelete = jest.fn()
-
-    const component = render(<Blog blog={blog1} user={geraldine} updateBlog={mockUpdate} deleteBlog={mockDelete} />)
+    const component = render(<Blog blog={blog1} user={geraldine} />)
     const blogDiv = component.container
     const blogInfo = component.container.querySelector('.blogInfo')
 
     expect(blogDiv).toHaveTextContent(`${blog1.title} - ${blog1.author}`)
     expect(blogInfo).toHaveStyle('display: none')
+  })
+  test('clicking the `view` button shows the blog`s likes and url', () => {
+
+    const component = render(<Blog blog={blog1} user={geraldine} />)
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    const blogInfo = component.container.querySelector('.blogInfo')
+
+    expect(blogInfo).not.toHaveStyle('display: none')
+    expect(blogInfo).toHaveTextContent(blog1.likes)
+    expect(blogInfo).toHaveTextContent(blog1.url)
   })
 })
