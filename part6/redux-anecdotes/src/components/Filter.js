54de89e7655e9360adac5d5e4409adffb5d8c081
circius <circius@posteo.de>
2020-12-05 20:15:02 +0100

    Filter with redux.connect

--- part6/redux-anecdotes/src/components/Filter.js
+++ part6/redux-anecdotes/src/components/Filter.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { setFilter } from '../reducers/filterReducer'
 
-const Filter = () => {
-  const dispatch = useDispatch()
+const Filter = props => {
+
   const handleChange = (event) => {
-    dispatch(setFilter(event.target.value))
+    props.setFilter(event.target.value)
   }
   const style = {
     marginBottom: 10
@@ -17,4 +17,4 @@ const Filter = () => {
     </div>)
 }
 
-export default Filter
+export default connect(null, { setFilter })(Filter)
