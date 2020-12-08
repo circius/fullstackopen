40424ceee180cf37b811a4fc5b706b0536ced186
circius <circius@posteo.de>
2020-12-08 13:19:34 +0100

    fix cookie helpers

--- part7/bloglist-frontend-redux/src/helpers/cookies.js
+++ part7/bloglist-frontend-redux/src/helpers/cookies.js
@@ -1,3 +1,5 @@
-export const getUserCookie = userTokenKey => window.localStorage.getItem(userTokenKey)
-export const setUserCookie = (userTokenKey, userJSON) => window.localStorage.setItem(userTokenKey, userJSON)
-export const unsetUserCookie = userTokenKey => window.localStorage.removeItem(userTokenKey)
+import { userTokenKey } from '../config'
+
+export const getUserCookie = () => window.localStorage.getItem(userTokenKey)
+export const setUserCookie = (userJSON) => window.localStorage.setItem(userTokenKey, userJSON)
+export const unsetUserCookie = () => window.localStorage.removeItem(userTokenKey)
