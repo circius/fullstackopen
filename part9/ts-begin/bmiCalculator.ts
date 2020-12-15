009645f2239ecb350e6429a46ba63c4346e26574
circius <circius@posteo.de>
2020-12-15 18:36:57 +0100

    fix bmiCalculator for terminal

--- part9/ts-begin/bmiCalculator.ts
+++ part9/ts-begin/bmiCalculator.ts
@@ -1,6 +1,32 @@
 
+interface Inputs {
+    height: number;
+    weight: number;
+}
+
 const HEALTHY = "Normal (healthy weight)"
 const UNHEALTHY = "Abnormal (unhealthy weight)"
+
+
+// consumes a list of strings (commandline arguments)
+// and produces valid inputs for bmiCalculator, Inputs.
+const parseArguments = (args: string[]): Inputs => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    const [height, weight] = [Number(args[2]), Number(args[3])]
+
+    if (!isNaN(height) && !isNaN(weight)) {
+        return {
+            height, weight
+        }
+    } else {
+        throw new Error('Invalid inputs!');
+    }
+}
+
+const { height, weight } = parseArguments(process.argv)
+
 
 // consumes a height (cm) and a weight (kgs) and produces a string
 // representing the corresponding health.
@@ -22,4 +48,4 @@ const bmiCalculator = (height: number, weight: number): string => {
     }
 }
 
-console.log(bmiCalculator(180, 74))
+console.log(bmiCalculator(height, weight))
