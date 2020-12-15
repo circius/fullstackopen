d1b41a67259c5b960fb028bf3b60d5ef1a3a97f9
circius <circius@posteo.de>
2020-12-15 18:55:09 +0100

    fix exerciseCalculator for terminal inputs

--- part9/ts-begin/exerciseCalculator.ts
+++ part9/ts-begin/exerciseCalculator.ts
@@ -13,11 +13,42 @@ enum Rating {
     Excellent
 }
 
+interface trainingInputs {
+    target: number;
+    trainingRecord: number[];
+}
+
 const DESCRIPTIONS =
     ["no-one is this bad",
         "you should try harder or reduce your target",
         "things are going ok",
         "you should increase your target"]
+
+// consumes a list of strings (commandline arguments)
+// and produces valid inputs for calculateExercises, Inputs.
+const parseTrainingArguments = (args: string[]): trainingInputs => {
+    // consumes a number (note that NaN is a number) and produces
+    // true if it's not NaN, false otherwise
+    const isNotNaN = (number: number): Boolean => {
+        return !isNaN(Number(number))
+    }
+    // consumes a list of numbers and produces true if none is NaN,
+    // false otherwise. (note that NaN is a number)
+    const validateArgs = (args: number[]): Boolean => {
+        return args.reduce((acc, cur) => isNotNaN(cur) && acc, true)
+    }
+    if (args.length < 4) throw new Error('Not enough arguments');
+    const maybeNums: number[] = args.map(arg => Number(arg))
+
+    if (validateArgs(maybeNums)) {
+        return {
+            target: maybeNums[0],
+            trainingRecord: maybeNums.slice(1)
+        }
+    } else {
+        throw new Error('invalid arguments')
+    }
+}
 
 // consumes a number representing average daily exercise and 
 // produces a Rating corresponding to it.
@@ -53,6 +84,7 @@ const calculateExercises = (actual: number[], target: number): Training => {
     }
 }
 
-const test_hours = [3, 0, 2, 4.5, 0, 3, 1]
-const test_target = 2
-console.log(calculateExercises(test_hours, test_target))
+const pertinentArgs = process.argv.slice(2)
+const { target, trainingRecord } = parseTrainingArguments(pertinentArgs)
+
+console.log(calculateExercises(trainingRecord, target))
