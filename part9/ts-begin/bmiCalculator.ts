70fa9e8628f2854488ed04368fd593ab6bf54016
circius <circius@posteo.de>
2020-12-15 17:57:25 +0100

    fix broken formula

--- part9/ts-begin/bmiCalculator.ts
+++ part9/ts-begin/bmiCalculator.ts
@@ -4,12 +4,14 @@ const UNHEALTHY = "Abnormal (unhealthy weight)"
 
 // consumes a height (cm) and a weight (kgs) and produces a string
 // representing the corresponding health.
+// bmiCalculator(5,5) -> UNHEALTHY
+// bmiCalculator(180,74) -> HEALTHY
 const bmiCalculator = (height: number, weight: number): string => {
     const healthyBmiP = (bmi: number): Boolean => {
         return 18.5 < bmi && bmi < 24.9
     }
-
-    const bmi: number = (height * height) / weight
+    const heightM: number = height / 100
+    const bmi: number = weight / (heightM * heightM)
     const healthy: Boolean = healthyBmiP(bmi)
 
     switch (healthy) {
@@ -20,4 +22,4 @@ const bmiCalculator = (height: number, weight: number): string => {
     }
 }
 
-console.log(bmiCalculator(5, 5))
+console.log(bmiCalculator(180, 74))
