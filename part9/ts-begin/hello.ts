1e21004d391cf1e2d9df14156a5b2d466af0ba3d
circius <circius@posteo.de>
2020-12-16 12:25:13 +0100

    solution to 9.1

--- part9/ts-begin/hello.ts
+++ part9/ts-begin/hello.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { bmiCalculator, Inputs as bmiInputs } from './bmiCalculator'
 
 const app = express();
 
@@ -6,6 +7,27 @@ const PORT = 3003;
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!')
+})
+
+app.get('/bmi', (req, res) => {
+    type bmiQuery = { height: string, weight: string }
+    const parseBmiQuery = (query: bmiQuery): bmiInputs => {
+        const { height, weight } = query
+        if (!isNaN(Number(height)) && !isNaN(Number(weight))) {
+            return { height: Number(height), weight: Number(weight) }
+        } else {
+            throw new Error('invalid query!')
+        }
+    }
+
+    try {
+        const { height, weight } = parseBmiQuery(req.query as bmiQuery)
+        const bmi = bmiCalculator(height, weight)
+        res.json({ height, weight, bmi })
+    } catch (exception) {
+        res.status(400).json({ error: "invalid or missing parameters" })
+    }
+
 })
 
 app.listen(PORT, () => {
