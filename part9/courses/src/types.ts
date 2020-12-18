076115077dbe13adcb20f9f75ea92bcf9e27afc1
circius <circius@posteo.de>
2020-12-18 22:18:51 +0100

    new types and adjusted data

--- part9/courses/src/types.ts
+++ part9/courses/src/types.ts
@@ -1,4 +1,25 @@
-export interface Course {
-    name: string,
-    exerciseCount: number
+interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
 }
+
+interface CoursePartDesc extends CoursePartBase {
+    description: string;
+}
+
+interface CoursePartOne extends CoursePartDesc {
+    name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+    name: "Using props to pass data";
+    groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartDesc {
+    name: "Deeper type usage";
+    exerciseSubmissionLink: string;
+}
+
+export type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree;
+
