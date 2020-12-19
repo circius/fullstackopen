41a7a04277fc501c3c42be6ecd3ad0b15b4c70fa
circius <circius@posteo.de>
2020-12-19 12:00:27 +0100

    add routers for /api/patients/:id

--- part9/patientor/src/routers/patient.ts
+++ part9/patientor/src/routers/patient.ts
@@ -1,6 +1,6 @@
 import Router from 'express';
 import patientService from '../services/patient';
-import { NonSensitivePatient, NewPatient } from '../types';
+import { NonSensitivePatient, Patient, NewPatient } from '../types';
 import { toNewPatient } from '../utility';
 
 const patientRouter = Router();
@@ -15,9 +15,15 @@ patientRouter.post('/', (req, res) => {
         const newPatient: NewPatient = toNewPatient(req.body);
         const addedEntry = patientService.addPatient(newPatient);
         res.json(addedEntry);
-    } catch (e) {
-        res.status(400).send(e.message);
+    } catch ({ message }) {
+        res.status(400).send(message);
     }
+});
+
+patientRouter.get('/:id', (req, res) => {
+    const id: string = req.params.id;
+    const patient: Patient | undefined = patientService.getPatient(id);
+    res.json(patient);
 });
 
 export = patientRouter;
