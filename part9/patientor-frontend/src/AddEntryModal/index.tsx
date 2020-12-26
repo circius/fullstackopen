0e3582dbb7ddb5d2ad13b9d2c076269f2021d5f6
circius <circius@posteo.de>
2020-12-26 19:23:34 +0100

    add form selection to form modal

--- part9/patientor-frontend/src/AddEntryModal/index.tsx
+++ part9/patientor-frontend/src/AddEntryModal/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Modal, Segment } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Modal, Segment, Button } from 'semantic-ui-react';
 import AddEntryForm, { EntryFormValues } from './AddEntryForm';
+import { NewEntryTypes } from '../types'
+import { HealthCheckIcon, HospitalIcon, OccupationalHealthcareIcon } from '../Icons';
 
 interface Props {
     modalOpen: boolean;
@@ -9,14 +11,26 @@ interface Props {
     error?: string;
 }
 
-const AddEntryModal = ({ modalOpen, onClose, onSubmit, error }: Props) => (
-    <Modal open={modalOpen} onClose={onClose} centered={false} closeIcon>
-        <Modal.Header>Add a new entry</Modal.Header>
-        <Modal.Content>
-            {error && <Segment inverted color="red">{`Error: ${error}`}</Segment>}
-            <AddEntryForm onSubmit={onSubmit} onCancel={onClose} />
-        </Modal.Content>
-    </Modal>
-);
+const AddEntryModal = ({ modalOpen, onClose, onSubmit, error }: Props) => {
+    const [formType, setFormType] = useState<NewEntryTypes>({ type: "Hospital" });
+    return (
+        <Modal open={modalOpen} onClose={onClose} centered={false} closeIcon>
+            <Modal.Header>Add a new entry</Modal.Header>
+            <Modal.Content>
+                <Button.Group horizontal labeled icon>
+                    <Button onClick={() => setFormType({ type: "Hospital" })}>{HospitalIcon} Hospital</Button>
+                    <Button onClick={() => setFormType({ type: "HealthCheck" })}>{HealthCheckIcon} Healthcheck </Button>
+                    <Button onClick={() => setFormType({ type: "OccupationalHealthcare" })}>{OccupationalHealthcareIcon}Occupational Health</Button>
+                </Button.Group>
+            </Modal.Content>
+            <Modal.Content>
+                {error && <Segment inverted color="red">{`Error: ${error}`}</Segment>}
+                <AddEntryForm onSubmit={onSubmit} onCancel={onClose} EntryType={formType} />
+
+
+            </Modal.Content>
+        </Modal>
+    )
+};
 
 export default AddEntryModal;
