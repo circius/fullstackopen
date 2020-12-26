854c2f2cd230e8e7ddfb060b344e5c763826a39b
circius <circius@posteo.de>
2020-12-26 19:23:20 +0100

    factor out icons

--- part9/patientor-frontend/src/PatientInfoPage/EntryCard/EntryCard.tsx
+++ part9/patientor-frontend/src/PatientInfoPage/EntryCard/EntryCard.tsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import { Icon } from 'semantic-ui-react';
+import { HealthCheckIcon, HospitalIcon, OccupationalHealthcareIcon } from '../../Icons';
 import { HealthCheckEntry, healthRating, OccupationalHealthcareEntry, HospitalEntry } from '../../types'
 
 import BaseEntryCard from './BaseEntryCard'
 
 export const HospitalEntryCard: React.FC<{ entry: HospitalEntry }> = ({ entry }) => (
-    <BaseEntryCard entry={entry} metaString="Hospital visit" icon={<Icon name="hospital" />}>
+    <BaseEntryCard entry={entry} metaString="Hospital visit" icon={HospitalIcon}>
         {entry.discharge ? `discharged on ${entry.discharge.date} on grounds: ${entry.discharge.criteria}` : null}
     </BaseEntryCard>
 )
 
 export const HealthCheckEntryCard: React.FC<{ entry: HealthCheckEntry }> = ({ entry }) => (
-    <BaseEntryCard entry={entry} metaString="Healthcheck" icon={<Icon name="heart outline" />}>
+    <BaseEntryCard entry={entry} metaString="Healthcheck" icon={HealthCheckIcon}>
         <HealthCheckRating rating={entry.healthCheckRating} />
     </BaseEntryCard>)
 
 export const OccupationalHealthcareEntryCard: React.FC<{ entry: OccupationalHealthcareEntry }> = ({ entry }) =>
-    <BaseEntryCard entry={entry} metaString="Occupational Healthcare" icon={<Icon name="stethoscope" />}>
+    <BaseEntryCard entry={entry} metaString="Occupational Healthcare" icon={OccupationalHealthcareIcon}>
         Employer: {entry.employerName}
     </BaseEntryCard>
 
