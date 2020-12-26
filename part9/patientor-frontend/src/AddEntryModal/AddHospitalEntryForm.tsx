d2a751a601f9e5e795859524575c45ee53483490
circius <circius@posteo.de>
2020-12-26 19:24:12 +0100

    working HospitalForm

---
+++ part9/patientor-frontend/src/AddEntryModal/AddHospitalEntryForm.tsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import { Grid, Button } from "semantic-ui-react";
+import { Field, Formik, Form } from "formik";
+import { TextField, DiagnosisSelection } from "../components/FormField";
+import { HospitalEntry } from '../types'
+import { useStateValue } from '../state';
+
+type EntryFormValues = Omit<HospitalEntry, "id">
+
+interface Props {
+    onSubmit: (values: EntryFormValues) => void;
+    onCancel: () => void;
+}
+const AddHospitalEntryForm: React.FC<Props> = ({ onSubmit, onCancel }) => {
+    const [{ diagnoses }] = useStateValue()
+    return (
+        <Formik
+            initialValues={{
+                type: "Hospital",
+                date: "",
+                specialist: "",
+                description: "",
+                diagnosisCodes: [],
+                discharge: {
+                    date: "",
+                    criteria: ""
+                },
+
+            }}
+            onSubmit={onSubmit}
+            validate={values => {
+                const requiredError = "Field is required";
+                const errors: { [field: string]: string } = {};
+
+                if (!values.date) {
+                    errors.date = requiredError;
+                }
+                if (!values.specialist) {
+                    errors.specialist = requiredError;
+                }
+                if (!values.description) {
+                    errors.description = requiredError;
+                }
+
+                return errors;
+            }}
+        >
+            {({ isValid, dirty, setFieldValue, setFieldTouched }) => {
+                return (
+                    <Form className="form ui">
+                        <Field
+                            label="Date"
+                            placeholder="YYYY-MM-DD"
+                            name="date"
+                            component={TextField}
+                        />
+                        <Field
+                            label="specialist"
+                            placeholder="name"
+                            name="specialist"
+                            component={TextField}
+                        />
+                        <Field
+                            label="description"
+                            placeholder="what happened?"
+                            name="description"
+                            component={TextField}
+                        />
+                        <Field
+                            label="Occupation"
+                            placeholder="Occupation"
+                            name="occupation"
+                            component={TextField}
+                        />
+                        <DiagnosisSelection
+                            setFieldValue={setFieldValue}
+                            setFieldTouched={setFieldTouched}
+                            diagnoses={Object.values(diagnoses)}
+                        />
+                        <Grid>
+                            <Grid.Column floated="left" width={5}>
+                                <Button type="button" onClick={onCancel} color="red">
+                                    Cancel
+                  </Button>
+                            </Grid.Column>
+                            <Grid.Column floated="right" width={5}>
+                                <Button
+                                    type="submit"
+                                    floated="right"
+                                    color="green"
+                                    disabled={!dirty || !isValid}
+                                >
+                                    Add
+                  </Button>
+                            </Grid.Column>
+                        </Grid>
+                    </Form>
+                );
+            }}
+        </Formik>
+    );
+};
+
+export default AddHospitalEntryForm
