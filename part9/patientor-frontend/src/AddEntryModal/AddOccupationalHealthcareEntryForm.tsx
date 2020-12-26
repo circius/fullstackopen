37ff9fcc1e3ea344270a1563291752f1dd743494
circius <circius@posteo.de>
2020-12-26 19:24:38 +0100

    template for OccupationalHealthcareEntryForm

---
+++ part9/patientor-frontend/src/AddEntryModal/AddOccupationalHealthcareEntryForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { ChildProps } from './AddEntryForm'
+
+
+const AddOccupationalHealthcareEntryForm: React.FC<ChildProps> = () => (
+    <div>to be implemented</div>
+)
+
+export default AddOccupationalHealthcareEntryForm
