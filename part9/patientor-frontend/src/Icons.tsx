a6148b858d1804e400971bf211e69bdf2e250738
circius <circius@posteo.de>
2020-12-26 19:23:11 +0100

    factor out Icons

---
+++ part9/patientor-frontend/src/Icons.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Icon } from 'semantic-ui-react'
+
+export const HospitalIcon = <Icon name="hospital" />
+export const HealthCheckIcon = <Icon name="heart outline" />
+export const OccupationalHealthcareIcon = <Icon name="stethoscope" />
